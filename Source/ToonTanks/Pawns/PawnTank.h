6358590ff48cd23f04956c8748bc18f687dbbd55
Rob <rob@gamedev.tv>
2020-07-21 22:16:29 +0100

    GameMode - ActorDied

--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -42,14 +42,11 @@ public:
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+	virtual void HandleDestruction() override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-	virtual void HandleDestruction() override; 
-
 };
