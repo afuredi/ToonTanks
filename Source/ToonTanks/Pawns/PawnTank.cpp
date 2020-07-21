45d8540f3620238e5b2c6625c383bf3ae96be6f3
Rob <rob@gamedev.tv>
2020-07-21 18:12:18 +0100

    Pawn Movement Input

--- Source/ToonTanks/Pawns/PawnTank.cpp
+++ Source/ToonTanks/Pawns/PawnTank.cpp
@@ -11,7 +11,7 @@ APawnTank::APawnTank()
     SpringArm->SetupAttachment(RootComponent);
 
     Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-    Camera->SetupAttachment(RootComponent);
+    Camera->SetupAttachment(SpringArm);
 }
 
 // Called when the game starts or when spawned
@@ -24,10 +24,35 @@ void APawnTank::BeginPlay()
 void APawnTank::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+    Rotate();
+    Move();
 }
 
 // Called to bind functionality to input
 void APawnTank::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
+    PlayerInputComponent->BindAxis("MoveForward", this, &APawnTank::CalculateMoveInput);
+    PlayerInputComponent->BindAxis("Turn", this, &APawnTank::CalculateRotateInput);
+}
+void APawnTank::CalculateMoveInput(float Value) 
+{
+    MoveDirection = FVector(Value * MoveSpeed * GetWorld()->DeltaTimeSeconds, 0, 0);
+}
+
+void APawnTank::CalculateRotateInput(float Value) 
+{
+    float RotateAmount = Value * RotateSpeed * GetWorld()->DeltaTimeSeconds;
+    FRotator Rotation = FRotator(0, RotateAmount, 0);
+    RotationDirection = FQuat(Rotation);
+}
+
+void APawnTank::Move() 
+{
+    AddActorLocalOffset(MoveDirection, true);
+}
+
+void APawnTank::Rotate() 
+{
+    AddActorLocalRotation(RotationDirection, true);
 }
--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -15,10 +15,23 @@ class TOONTANKS_API APawnTank : public APawnBase
 	GENERATED_BODY()
 
 private:
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* SpringArm;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* Camera; 
+
+	FVector MoveDirection;
+	FQuat RotationDirection;
+
+	float MoveSpeed = 100.0f;
+	float RotateSpeed = 100.0f;
+
+	void CalculateMoveInput(float Value);
+	void CalculateRotateInput(float Value);
+
+	void Move();
+	void Rotate();
 
 public:
 
