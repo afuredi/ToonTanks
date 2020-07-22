5246dcfed3effcd6bc61708d2f9d6aa673034f87
Rob <rob@gamedev.tv>
2020-07-22 12:31:20 +0100

    Implementing Camera Shake

--- Source/ToonTanks/Actors/ProjectileBase.h
+++ Source/ToonTanks/Actors/ProjectileBase.h
@@ -34,6 +34,9 @@ private:
 	USoundBase* HitSound;
 	UPROPERTY(EditAnywhere, Category = "Effects")
 	USoundBase* LaunchSound;
+	UPROPERTY(EditAnywhere, Category = "Effects")
+	TSubclassOf<UCameraShake> HitShake;
+
 	// FUNCTIONS
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
